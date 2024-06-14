65db14677d592356df1b3f83a3c2312801c46fbc
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-06-14 08:35:53 -0600

    Remove login config

--- src/main/java/com/tringuyen/minesweeper/security/WebSecurityConfig.java
+++ src/main/java/com/tringuyen/minesweeper/security/WebSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableMethodSecurity
@@ -49,12 +48,6 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                                                .permitAll()
                                                .anyRequest()
                                                .authenticated());
-        http.formLogin(auth -> auth.loginPage("/login")
-                                   .loginProcessingUrl("/login")
-                                   .permitAll());
-
-        http.logout(auth -> auth.deleteCookies("JSESSIONID")
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/")));
 
         http.authenticationProvider(authenticationProvider());
 
